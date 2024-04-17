b578129fcad6756c893dfdee125b194b5dd8b600
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-18 05:00:15 +0530

    ecom app header UI completed

--- Client/components/Layout/Header/Header.jsx
+++ Client/components/Layout/Header/Header.jsx
@@ -1,14 +1,29 @@
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Feather from "react-native-vector-icons/Feather";
 
 const Header = () => {
+  const[searchText,setSearchText]= useState("")
+  // function for search
+  const searchHandler = ()=>{
+    console.log(searchText)
+    setSearchText("")
+  }
   return (
     <View style={styles.box}>
       <View style={styles.container}>
-        <TextInput style={styles.InputBox}/>
-        <TouchableOpacity style={styles.searchBtn}>
-          <FontAwesome name="search"  style={styles.icon}/>
+        <Feather name="menu" style={styles.menuicon} />
+        <TextInput
+          style={styles.InputBox}
+          value={searchText}
+          placeholder="Iphone 14 max pro"
+          onChangeText={(e) => {
+            setSearchText(e);
+          }}
+        />
+        <TouchableOpacity style={styles.searchBtn} onPress={searchHandler}>
+          <FontAwesome name="search" style={styles.icon} />
         </TouchableOpacity>
       </View>
     </View>
@@ -19,10 +34,10 @@ export default Header
 
 const styles = StyleSheet.create({
   box: {
-    marginTop: 15,
-    height: 90,
+    marginTop: 20,
+    height: 60,
     // flex: 1,
-    backgroundColor: "#f0f5ff",
+    backgroundColor: "#23527c",
     // alignItems: "center",
     // justifyContent: "center",
   },
@@ -36,24 +51,29 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
   },
   InputBox: {
-    borderWidth: 0.8,
-    width: "100%",
+    // borderWidth: 1,
+    width: "94%",
     position: "absolute",
-    left: 15,
+    // left: 5,
+    right: 5,
     height: 40,
     color: "black",
     backgroundColor: "white",
-    paddingLeft: 10,
+    paddingLeft: 15,
     fontSize: 16,
-    borderRadius: 5,
+    borderRadius: 28,
   },
   searchBtn: {
     position: "absolute",
-    left: "95%",
+    left: "98%",
     // height
   },
   icon: {
     color: "black",
     fontSize: 18,
+  },
+  menuicon: {
+    color: "white",
+    fontSize: 25,
   },
 });
