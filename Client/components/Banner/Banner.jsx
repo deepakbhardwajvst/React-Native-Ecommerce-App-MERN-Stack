d9d88945a7f42175f52cb49d966332b5a854c82a
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-20 13:02:02 +0530

    bannerdata and banner comp created

---
+++ Client/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <View>
+      <Text>Banner</Text>
+    </View>
+  )
+}
+
+export default Banner
+
+const styles = StyleSheet.create({})
