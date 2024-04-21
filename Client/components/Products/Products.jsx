148afc7496c6b9a59aef20f0fa210ad6546836e1
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-21 20:40:35 +0530

    productData and productCard comp creaated

--- Client/components/Products/Products.jsx
+++ Client/components/Products/Products.jsx
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import ProductsCard from "./ProductsCard.jsx/index.js";
+import { ProductsData } from "../../data/ProductsData.js";
 
 const Products = () => {
   return (
     <View>
-      <Text>Products</Text>
+      {ProductsData.map((p) => (
+        <ProductsCard key={p._id} p={p} />
+      ))}
     </View>
-  )
-}
+  );
+};
 
-export default Products
-
-const styles = StyleSheet.create({})
+export default Products;
---
+++ Client/components/Products/ProductsCard.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProductsCard = () => {
+  return (
+    <View>
+      <Text>ProductsCard</Text>
+    </View>
+  )
+}
+
+export default ProductsCard
+
+const styles = StyleSheet.create({})
