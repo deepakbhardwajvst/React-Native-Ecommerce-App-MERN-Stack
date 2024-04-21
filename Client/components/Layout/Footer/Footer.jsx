6b9cf4cd0112b4f8deb027512d5a0f8ccb5d7544
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-21 22:46:54 +0530

    footer work done and added some icon and heading for them

--- Client/components/Layout/Footer/Footer.jsx
+++ Client/components/Layout/Footer/Footer.jsx
@@ -1,14 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import AntDesign from "react-native-vector-icons/AntDesign";
 const Footer = () => {
   return (
-    <View>
-      <Text>Footer</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.menuContainer}>
+        <AntDesign style={styles.icon} name="home" />
+        <Text style={styles.iconText}>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuContainer}>
+        <AntDesign style={styles.icon} name="bells" />
+        <Text style={styles.iconText}>notification</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuContainer}>
+        <AntDesign style={styles.icon} name="user" />
+        <Text style={styles.iconText}>Account</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuContainer}>
+        <AntDesign style={[styles.icon]} name="shoppingcart" />
+        <Text style={styles.iconText}>Cart</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuContainer}>
+        <AntDesign style={styles.icon} name="logout" />
+        <Text style={styles.iconText}>Logout</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Footer
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+  },
+  menuContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  icon: {
+    fontSize: 25,
+    color: "#000000",
+  },
+  iconText: {
+    color: "#000000",
+    fontSize: 10,
+  },
+  active: {
+    color: "blue",
+  },
+});
+export default Footer;
--- Client/components/Layout/Layout.jsx
+++ Client/components/Layout/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = ({children}) => {
     <StatusBar />
     <Header />
       <Text>{children}</Text>
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 } 
