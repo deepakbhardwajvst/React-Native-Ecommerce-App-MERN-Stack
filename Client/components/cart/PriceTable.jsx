c72e8fec475782962a8abe4bb70c71f2f52d36a4
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-05-15 04:36:52 +0530

    price table work done

--- Client/components/cart/PriceTable.jsx
+++ Client/components/cart/PriceTable.jsx
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
-const PriceTable = ({title,price}) => {
+const PriceTable = ({ price, title }) => {
   return (
-    <View>
-      <Text>PriceTable</Text>
+    <View style={styles.container}>
+      <Text>{title}</Text>
+      <Text>{price} $</Text>
     </View>
-  )
-}
+  );
+};
 
-export default PriceTable
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+    alignItems: "center",
+  },
+});
+export default PriceTable;
