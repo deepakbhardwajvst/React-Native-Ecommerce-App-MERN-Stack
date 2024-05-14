bf3be231216badaf1a3ced96504024bfcaba65c5
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-05-15 04:19:53 +0530

    cart data added

--- Client/screens/Cart/Cart.jsx
+++ Client/screens/Cart/Cart.jsx
@@ -1,15 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
+import {
+  View,
+  Text,
+  StyleSheet,
+//   ScrollView,
+//   TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import Layout from "../../components/Layout/Layout";
+import { CartData } from "../../data/CartData";
 
-const Cart = () => {
+const Cart = ({ navigation }) => {
+  const [cartItems, setCartItems] = useState(CartData);
   return (
     <Layout>
-      <Text>Cart</Text>
+     
+     <View>
+        <Text>cart</Text>
+     </View>
     </Layout>
   );
-}
-
-export default Cart
-
-const styles = StyleSheet.create({})
+};
+const styles = StyleSheet.create({
+  heading: {
+    textAlign: "center",
+    color: "green",
+    marginTop: 10,
+  },
+  grandTotal: {
+    borderWidth: 1,
+    borderColor: "lightgray",
+    backgroundColor: "#ffffff",
+    padding: 5,
+    margin: 5,
+    marginHorizontal: 20,
+  },
+  btnCheckout: {
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 40,
+    backgroundColor: "#000000",
+    width: "90%",
+    marginHorizontal: 20,
+    borderRadius: 20,
+  },
+  btnCheckoutText: {
+    color: "#ffffff",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
+export default Cart;
