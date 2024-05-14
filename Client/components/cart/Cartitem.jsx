a5b550c5439d3740794bb2dedbc0c606daec5dd5
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-05-15 04:38:42 +0530

    Cartitem comp created for showing added products

---
+++ Client/components/cart/Cartitem.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Cartitem = () => {
+  return (
+    <View>
+      <Text>Cartitem</Text>
+    </View>
+  )
+}
+
+export default Cartitem
+
+const styles = StyleSheet.create({})
