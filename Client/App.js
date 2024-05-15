fbd6b9de0bb16c4b00f7cb0ed4c1fa716ee7123e
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-05-16 05:13:06 +0530

    doing ui work for Notification section

--- Client/App.js
+++ Client/App.js
@@ -11,6 +11,7 @@ import Payments from "./screens/Payments/Payments";
 import Login from './screens/Auth/Login/Login';
 import Register from "./screens/Auth/Register/Register";
 import Account from "./screens/Account/Account";
+import Notification from "./screens/Account/Notification/Notification";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -56,6 +57,10 @@ export default function App() {
         <Stack.Screen
           name="account"
           component={Account}
+        />
+        <Stack.Screen
+          name="notification"
+          component={Notification}
         />
       </Stack.Navigator>
     </NavigationContainer>
