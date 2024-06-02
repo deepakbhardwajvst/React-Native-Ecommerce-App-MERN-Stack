31657c0c2cf6b6c3f2d986e3b4ebbd2d60c39a79
deepakbhardwajcms <deepak.bhardwaj@clingmultisolutions.org>
2024-06-02 16:19:59 +0530

    a

---
+++ Client/data/OrderData.js
@@ -0,0 +1,28 @@
+export const orderData = [
+  {
+    _id: 1,
+    totalAmount: 1001,
+    status: "processing",
+    date: "1st Nov",
+    paymentMode: "online",
+    productInfo: {
+      _id: 1,
+      name: "Iphone 15",
+      qty: 1,
+      price: 999,
+    },
+  },
+  {
+    _id: 2,
+    totalAmount: 1001,
+    status: "processing",
+    date: "1st Nov",
+    paymentMode: "online",
+    productInfo: {
+      _id: 2,
+      name: "Iphone 15",
+      qty: 2,
+      price: 999,
+    },
+  },
+];
