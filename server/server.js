ba4403cc4382861b76f10064c795072966f82191
deepakbhardwajcms <deepak.bhardwaj@clingmultisolutions.org>
2024-06-02 18:40:51 +0530

    setup node.js done install done of dotenv express cors morgan colors

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import colors from "colors"; // console per color
+import morgan from "morgan"; // localhost per huye refresh yah action ke liya
+import cors from "cors"; // so we dont get cross origin error to connect client an server
+import dotenv from "dotenv"; // managing sensitive data such as API keys, database credentials 
+
+// dot env config
+dotenv.config()
+
+
+// rest object
+const app = express()
+
+// middleware
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(cors())
+
+// route
+app.get('/', (req,res)=>{
+    return res.status(200).send("<h1>app.get ne kiya run Forward slash per </h1>")
+}) 
+
+// port
+const PORT = process.env.PORT || 4040;
+// listen
+app.listen(PORT,()=>{
+    console.log(`SERVER IS RUNNING ${process.env.PORT} `.bgMagenta.white) 
+})
