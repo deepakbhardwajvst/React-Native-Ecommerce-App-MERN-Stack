f51bc64f2b98f3d8829557d8110c09d8f3e61173
deepakbhardwajcms <deepak.bhardwaj@clingmultisolutions.org>
2024-06-08 05:16:36 +0530

    isAuth

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,3 +1,17 @@
+import JWT from "jsonwebtoken"
+import userModel from "../models/userModel"
+
+
 export const isAuth = async (req,res,next)=>{
     const {token} = req.cookies
+    // Validation
+    if (!token) {
+        return res.status(401).send({
+            success:false,
+            message:"Unauthorized User"
+        })   
+    }
+    const decodeData = JWT.verify(token, process.env.JWT_SECRET)
+    req.user = userModel.findById(decodeData._id)
+    next()
 }
