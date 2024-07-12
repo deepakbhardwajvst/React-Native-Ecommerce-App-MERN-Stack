8ddcd537f2c36623662becd9c6316d3c4293ad22
deepakbhardwajcms <deepak.bhardwaj@clingmultisolutions.org>
2024-07-12 13:29:54 +0530

    installed backend security package and also added limiter on login and register

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,12 +11,21 @@ import {
 } from "../controllers/userController.js";
 import { isAuth } from "../middlewares/authMiddleware.js";
 import { singleUpload } from "../middlewares/multer.js";
+import { rateLimit } from "express-rate-limit";
+// IP rate limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
 // router object
 const router = express.Router();
 // register api
-router.post("/register", registerController);
+router.post("/register",limiter, registerController);
 // login api
-router.post("/login", loginController);
+router.post("/login", limiter, loginController);
 // profile
 router.get("/profile",isAuth,getUserProfileController)
 // Logout
